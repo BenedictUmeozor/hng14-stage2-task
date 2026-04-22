263c16a0c4c8341a54ffe9462d49b2ea7c1d12f4
Benedict <benedictumeozor@gmail.com>
2026-04-22 11:25:12 +0100

    hover state fix

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -12,7 +12,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
         className={cn(
-          "heading-s-variant focus:border-02! text-08 dark:bg-03 dark:focus:border-04! caret-01 block h-12 w-full rounded border px-5 focus:outline-none dark:text-white",
+          "heading-s-variant hover:border-02! dark:hover:border-02! focus:border-02! text-08 dark:bg-03 dark:focus:border-02! caret-01 block h-12 w-full rounded border px-5 focus:outline-none dark:text-white",
           error ? "border-09!" : "border-05! dark:border-04!",
           className,
         )}
